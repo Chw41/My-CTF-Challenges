4eeeafe17b2dae9b6893e2a5eb61494994fdb91e
Chw41 <frank.chw41@gmail.com>
2024-04-19 20:15:44 +0800

    update jscalc topic

---
+++ HackTheBox/jscalc/challenge/helpers/calculatorHelper.js
@@ -0,0 +1,15 @@
+module.exports = {
+    calculate(formula) {
+        try {
+            return eval(`(function() { return ${ formula } ;}())`);
+
+        } catch (e) {
+            if (e instanceof SyntaxError) {
+                return 'Something went wrong!';
+            }
+        }
+    }
+}
+
+
+// ocd
