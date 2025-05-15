6b7cc1b6a1ba51fb314add8ef81884fc0e043ff7
frank.chw41@gmail.com <CWei@CHW-MacBook-Pro.local>
2025-05-15 20:06:50 +0800

    rm HTB

--- HackTheBox/HackTheBox_ jscalc/build-docker.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker build --tag=web_jscalc .
-docker run -p 1337:1337 --rm --name=web_jscalc -it web_jscalc
--- HackTheBox/hackTheBox_ Neonify/build-docker.sh
+++
@@ -1,2 +0,0 @@
-docker build -t web_neonify .
-docker run  --name=web_neonify --rm -p1337:1337 -it web_neonify
